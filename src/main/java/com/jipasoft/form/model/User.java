16cb7fc465eeb9251af678dea69fde02ba3e0b43
Julius Krah <juliuskrah@gmail.com>
2016-03-05 20:04:28 +0000

    added lombok to classpath

--- src/main/java/com/jipasoft/form/model/User.java
+++ src/main/java/com/jipasoft/form/model/User.java
@@ -2,149 +2,48 @@ package com.jipasoft.form.model;
 
 import java.util.List;
 
+import lombok.Data;
+
+@Data
 public class User {
 	// form:hidden - hidden value
-	Integer id;
+	private Integer id;
 
 	// form:input - textbox
-	String name;
+	private String name;
 
 	// form:input - textbox
-	String email;
+	private String email;
 
 	// form:textarea - textarea
-	String address;
+	private String address;
 
 	// form:input - password
-	String password;
+	private String password;
 
 	// form:input - password
-	String confirmPassword;
+	private String confirmPassword;
 
 	// form:checkbox - single checkbox
-	boolean newsletter;
+	private boolean newsletter;
 
 	// form:checkboxes - multiple checkboxes
-	List<String> framework;
+	private List<String> framework;
 
 	// form:radiobutton - radio button
-	String sex;
+	private String sex;
 
 	// form:radiobuttons - radio button
-	Integer number;
+	private Integer number;
 
 	// form:select - form:option - dropdown - single select
-	String country;
+	private String country;
 
 	// form:select - multiple=true - dropdown - multiple select
-	List<String> skill;
+	private List<String> skill;
 
 	public boolean isNew() {
 		return (this.id == null);
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getConfirmPassword() {
-		return confirmPassword;
-	}
-
-	public void setConfirmPassword(String confirmPassword) {
-		this.confirmPassword = confirmPassword;
-	}
-
-	public boolean isNewsletter() {
-		return newsletter;
-	}
-
-	public void setNewsletter(boolean newsletter) {
-		this.newsletter = newsletter;
-	}
-
-	public List<String> getFramework() {
-		return framework;
-	}
-
-	public void setFramework(List<String> framework) {
-		this.framework = framework;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
-	public Integer getNumber() {
-		return number;
-	}
-
-	public void setNumber(Integer number) {
-		this.number = number;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public List<String> getSkill() {
-		return skill;
-	}
-
-	public void setSkill(List<String> skill) {
-		this.skill = skill;
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address
-				+ ", password=" + password + ", confirmPassword=" + confirmPassword
-				+ ", newsletter=" + newsletter + ", framework=" + framework + ", sex=" + sex
-				+ ", number=" + number + ", country=" + country + ", skill=" + skill + "]" + isNew();
 	}
 
 }
